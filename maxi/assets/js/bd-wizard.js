064b186ac9e0c5616258df1d1f0ac4a0bd9a127f
nielsdonl <nielsdoornbos@outlook.com>
2022-03-23 10:37:09 +0100

    Update bd-wizard.js
    test jquery email link

--- maxi/assets/js/bd-wizard.js
+++ maxi/assets/js/bd-wizard.js
@@ -29,3 +29,14 @@ $('#doelgroep').on('change', function(e) {
 $('#Doel').on('change', function(e) {
     $('#ingevoerdeDoel').text(e.target.value || 'willms_abby@gmail.com');
 });
+
+$(function () {
+  $('#emailLink').on('click', function (event) {
+      event.preventDefault();
+    alert("Huh");
+    var email = 'test@theearth.com';
+    var subject = 'Circle Around';
+    var emailBody = 'Some blah';
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+  });
+});
